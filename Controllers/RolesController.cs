c490f2fec691eb4ba2383d04e3850e9820cc72f5
Obadiah Korir <korir400@gmail.com>
2024-02-08 16:37:25 +0300

    Latest Updates

---
+++ Controllers/RolesController.cs
@@ -0,0 +1,93 @@
+﻿using EmployeesManagement.Data;
+using EmployeesManagement.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Data;
+
+namespace EmployeesManagement.Controllers
+{
+    public class RolesController : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly RoleManager<IdentityRole> _rolemanager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly ApplicationDbContext _context;
+
+        public RolesController(ApplicationDbContext context, UserManager<IdentityUser> userManager, RoleManager<IdentityRole> rolemanager, SignInManager<IdentityUser> signInManager)
+        {
+            _rolemanager = rolemanager;
+
+            _signInManager = signInManager;
+
+            _userManager = userManager;
+
+            _context = context;
+        }
+        public async Task<ActionResult> Index()
+        {
+            var roles = await _context.Roles.ToListAsync();
+            return View(roles);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Create(RolesViewModel model)
+        {
+            IdentityRole role = new IdentityRole();
+            role.Name = model.RoleName;
+
+            var result = await _rolemanager.CreateAsync(role);
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View(model);
+            }
+
+
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> Edit(string id)
+        {
+            var role = new RolesViewModel();
+            var result = await _rolemanager.FindByIdAsync(id);
+            role.RoleName = result.Name;
+            role.Id = result.Id;
+
+            return View(role);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Edit(string id,RolesViewModel model)
+        {
+            var checkifexist = await _rolemanager.RoleExistsAsync(model.RoleName);
+            if (!checkifexist)
+            {
+
+                var result = await _rolemanager.FindByIdAsync(id);
+                result.Name = model.RoleName;
+
+                var finalresult = await _rolemanager.UpdateAsync(result);
+                if (finalresult.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    return View(model);
+                }
+            }
+
+            return View(model);
+        }
+    }
+}
---
+++ ViewModels/RolesViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace EmployeesManagement.ViewModels
+{
+    public class RolesViewModel
+    {
+        public string Id { get; set; }
+
+        public string RoleName { get; set; }
+    }
+}
